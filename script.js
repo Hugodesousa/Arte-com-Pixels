45199334982d00129d12b9634c1dc9b94e400f82
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-21 01:45:17 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+const button1 = document.getElementById('color1');
+const button2 = document.getElementById('color2');
+const button3 = document.getElementById('color3');
+const button4 = document.getElementById('color4');
+button1.style.backgroundColor = 'black';
+button2.style.backgroundColor = 'rgb(158, 1, 1)';
+button3.style.backgroundColor = 'rgb(236, 204, 18)';
+button4.style.backgroundColor = 'rgb(13, 177, 48)';
+button1.classList.add('selected');
+
 function selectColor(Event) {
-  const selet = document.querySelector('.selected');
-  // console.log(selet);
-  selet.classList.remove('.selected');
+  const cor = document.querySelector('.selected');
+  cor.classList.remove('selected');
   Event.target.classList.add('selected');
 }
 
-const button1 = document.getElementById('color1');
 button1.addEventListener('click', selectColor);
-
-const button2 = document.getElementById('color2');
 button2.addEventListener('click', selectColor);
-
-const button3 = document.getElementById('color3');
 button3.addEventListener('click', selectColor);
-
-const button4 = document.getElementById('color4');
 button4.addEventListener('click', selectColor);
