fbec79b40b480be5a125168f33b37edb11f67e42
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-21 12:19:47 -0300

    ids para cada pixel

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const button1 = document.getElementById('color1');
 const button2 = document.getElementById('color2');
 const button3 = document.getElementById('color3');
 const button4 = document.getElementById('color4');
+
 button1.style.backgroundColor = 'rgb(0, 0, 0)';
-button2.style.backgroundColor = 'rgb(158, 1, 1)';
+button2.style.backgroundColor = 'rgb(158, 1, 20)';
 button3.style.backgroundColor = 'rgb(236, 204, 18)';
 button4.style.backgroundColor = 'rgb(13, 177, 48)';
 button1.classList.add('selected');
@@ -26,6 +27,7 @@ function creatPixel() {
     const pixel = document.createElement('div');
     board.appendChild(pixel);
     pixel.classList.add('pixel');
+    pixel.id = 'pix' + parseInt(index);
   }
 }
 // eslint-disable-next-line sonarjs/no-use-of-empty-return-value
