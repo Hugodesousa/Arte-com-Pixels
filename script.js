38ad44575447c359a178c0fc8d68596dab9a6393
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-22 14:58:57 -0300

    botão clear , cada pixel a seecionado ao clicar

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// Declara botoes da palheta de cores como const.
 const button1 = document.getElementById('color1');
 const button2 = document.getElementById('color2');
 const button3 = document.getElementById('color3');
 const button4 = document.getElementById('color4');
-
+const clear = document.getElementById('clear-board');
+// Determina cores da palheta.
 button1.style.backgroundColor = 'rgb(0, 0, 0)';
 button2.style.backgroundColor = 'rgb(158, 1, 20)';
 button3.style.backgroundColor = 'rgb(236, 204, 18)';
 button4.style.backgroundColor = 'rgb(13, 177, 48)';
 button1.classList.add('selected');
-
+// remove  a classe selected e adiciona a mesma na cor selecionada.
 function selectColor(Event) {
   const cor = document.querySelector('.selected');
   cor.classList.remove('selected');
@@ -20,6 +22,8 @@ button2.addEventListener('click', selectColor);
 button3.addEventListener('click', selectColor);
 button4.addEventListener('click', selectColor);
 
+// Cria 25 divs para represeratam os pixel a serem coloridos ( adiciona uma classa para todas as divs e um id para cada pixel).
+
 const board = document.getElementById('pixel-board');
 
 function creatPixel() {
@@ -27,16 +31,27 @@ function creatPixel() {
     const pixel = document.createElement('div');
     board.appendChild(pixel);
     pixel.classList.add('pixel');
-    pixel.id = 'pix' + parseInt(index);
+    pixel.id = `px${index}`;
   }
 }
 // eslint-disable-next-line sonarjs/no-use-of-empty-return-value
 window.onload = creatPixel();
 
-const pix = document.getElementsByClassName('pixel');
-function color() {
-  
-  console.log(pix)
+// Adiciona cor ao pixel clicado.
+//
 
+const px0 = document.querySelectorAll('.pixel');
+for (let i = 0; i < px0.length; i += 1){
+  px0[i].addEventListener('click',function seler (){
+    px0[i].style.backgroundColor = 'red';
+  })
 }
- board.addEventListener('click', color)
+console.log(clear);
+
+// Funçaão para limpar board.
+function cle(){
+  for (let i = 0; i < px0.length; i += 1){
+  px0[i].style.backgroundColor = 'white'
+  }
+}
+clear.addEventListener('click', cle);
